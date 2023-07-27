1c80f81092d32f63fc605192412c936a16120ebb
Vincent Lin <vincentlin1617@gmail.com>
2023-07-27 13:51:13 -0600

    Added in sase colors, pages, and cleaned up the deafult template

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>This is the login page of the app</div>
+  )
+}
+
+export default Login
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Login from './login/page'
 
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
       <h1>Hello Rachel</h1>
       <h4>This is the site and everything is deployed</h4>
       <h6>Now we just need to work on it</h6>
+      <Login/>
     </main>
   )
 }
